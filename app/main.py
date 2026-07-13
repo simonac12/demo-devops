7fa0a6f00c83f4fc82c6b4419b6caea65878bcda
simona <simona.cavalus@vodafone.com>
2026-07-13 21:30:25 +0300

    feat: add an endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import time
 from prometheus_fastapi_instrumentator import Instrumentator
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -10,19 +11,10 @@ Instrumentator().instrument(app).expose(app)
 def home():
     return {"message": "Welcome to DevOps Demo"}
 
-@app.get("/products")
-def products():
-    return [
-        {"id": 1, "name": "Laptop"},
-        {"id": 2, "name": "Mouse"},
-        {"id": 3, "name": "Keyboard"},
-    ]
+from fastapi import HTTPException
 
-@app.get("/slow")
-def slow():
-    time.sleep(2)
-    return {"message": "This endpoint is intentionally slow"}
+@app.get("/buy")
+def buy():
+    raise HTTPException(status_code=500, detail="Database unavailable")
 
-@app.get("/error")
-def error():
-    raise Exception("Something went wrong")
+
