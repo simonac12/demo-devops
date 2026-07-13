6ae01139b87fd7279c36a06707b6468066b63373
simona <simona.cavalus@vodafone.com>
2026-07-13 21:34:10 +0300

    fix: delete duplicates

--- app/main.py
+++ app/main.py
@@ -11,8 +11,6 @@ Instrumentator().instrument(app).expose(app)
 def home():
     return {"message": "Welcome to DevOps Demo"}
 
-from fastapi import HTTPException
-
 @app.get("/buy")
 def buy():
     raise HTTPException(status_code=500, detail="Database unavailable")
